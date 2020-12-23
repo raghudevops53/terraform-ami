88dcb50e79247b804d44c47853a9e929f21abb38
KalluruS <>
2020-12-23 08:43:06 +0530

    Update terraform-project/terraform-ami/

---
+++ instance.tf
@@ -0,0 +1,7 @@
+resource "aws_instance" "ami_instance" {
+  ami                     = data.aws_ami.ami.id
+  instance_type           = "t3.small"
+  tags                    = {
+        Name              = "${var.COPMPONENT}-ami"
+  }
+}
