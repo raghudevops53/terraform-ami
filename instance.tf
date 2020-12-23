fab37ac3663925797ab67a116fe3efb15433c8ef
KalluruS <>
2020-12-23 08:50:23 +0530

    Update terraform-project/terraform-ami/

--- instance.tf
+++ instance.tf
@@ -6,3 +6,18 @@ resource "aws_instance" "ami_instance" {
   }
 }
 
+resource "null_resource" "provisioner" {
+  provisioner "remote-exec" {
+    connection {
+      host              = aws_instance.ami_instance.public_ip
+      user              = "root"
+      password          = "DevOps321" // Hardcoding the username and password in code is the worst idea and it causes security breaches as well
+    }
+    inline = [
+      "yum install make -y",
+      "git clone https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps53/_git/shell-scripting",
+      "cd shell-scripting/roboshop-project"
+      "make ${var.COMPONENT}"
+    ]
+  }
+}
