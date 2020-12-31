b45b90085aa6ace9d836192b4cab9bd53ec6f4f2
KalluruS <>
2020-12-31 07:50:26 +0530

    Update terraform-project/terraform-ami/

--- state.tf
+++ state.tf
@@ -1,11 +1,6 @@
-//terraform {
-//  backend "s3" {
-//    bucket          = "d53-terraform-state-files"
-//    key             = "ami/terraform.tfstate"
-//    region          = "us-east-1"
-//    dynamodb_table  = "terraform"
-//  }
-//}
+terraform {
+  backend "s3" {}
+}
 
 provider "aws" {
   region = "us-east-1"
