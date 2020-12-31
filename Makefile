2018524343f6e769f21719cc3272545ba4904b89
KalluruS <>
2020-12-31 07:44:05 +0530

    Update terraform-project/terraform-ami/

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ help:           ## Show this help.
 	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
 
 init: ## Terraform init for DEV env
+	@echo ${COMPONENT}
 	@terraform init -no-color
 
 apply: ## Terraform Apply for DEV env
--- state.tf
+++ state.tf
@@ -1,12 +1,13 @@
-terraform {
-  backend "s3" {
-    bucket          = "d53-terraform-state-files"
-    key             = "ami/terraform.tfstate"
-    region          = "us-east-1"
-    dynamodb_table  = "terraform"
-  }
-}
+//terraform {
+//  backend "s3" {
+//    bucket          = "d53-terraform-state-files"
+//    key             = "ami/terraform.tfstate"
+//    region          = "us-east-1"
+//    dynamodb_table  = "terraform"
+//  }
+//}
 
 provider "aws" {
   region = "us-east-1"
 }
+
